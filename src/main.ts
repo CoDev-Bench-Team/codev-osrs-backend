6103d945ab5caf9bb48fa42baab10c59de27984b
Christian Ted Ochoa <christianted.ochoa@gmail.com>
2026-09-14 22:42:51 +0800

    Revert "Add debug logs to the 'main.ts' file"
    This reverts commit 5c0a942e4ae04cb5fbf46c3289ce982dc35d602c.

--- src/main.ts
+++ src/main.ts
@@ -3,31 +3,10 @@ import { AppModule, ObserveInstrument } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  console.log('[bootstrap] Starting NestJS', {
-    node: process.version,
-    hasDatabaseUrl: Boolean(process.env.POSTGRESQL_HOST),
-  });
-  
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
-
-  console.log('[bootstrap] NestJS application created');
-
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
-
-  console.log('[bootstrap] NestJS listening');
 }
-
-bootstrap().catch((error) => {
-  console.error('[bootstrap] FAILED');
-
-  if (error instanceof Error) {
-    console.error(error.stack);
-  } else {
-    console.error(error);
-  }
-
-  process.exitCode = 1;
-});
+await bootstrap();
