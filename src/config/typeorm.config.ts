b4039eb2580d4e7acbafb62856e213ab105f53a5
Christian Ted Ochoa <christianted.ochoa@gmail.com>
2026-09-14 22:42:15 +0800

    Revert "Add a static 'pg' import"
    This reverts commit ac8c5d05775fa9f6863cad3ee66a4149ffc26885.

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -3,9 +3,6 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { DataSource } from 'typeorm';
 
-// static import so Vercel's file tracer bundles pg (TypeORM loads it dynamically otherwise)
-import 'pg';
-
 config();
 
 const __filename = fileURLToPath(import.meta.url);
