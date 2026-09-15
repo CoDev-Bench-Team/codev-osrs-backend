8e5e3bf53e552229fd201749a00f22917b0b8d8c
Christian Ted Ochoa <christianted.ochoa@gmail.com>
2026-09-15 09:51:01 +0800

    Integrate Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('CoDev OSRS API')
+    .setDescription('API documentation for the CoDev OSRS backend')
+    .setVersion('1.0')
+    .addTag('CoDev OSRS')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
